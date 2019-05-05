fe839e0b905ce820c7881fcbb0d38cc12eb1b851
MilanZlaja1986 <milan.zlatkovic@hotmail.rs>
2019-05-05 16:07:39 +0200

    Second change

--- src/optional/Validation.java
+++ src/optional/Validation.java
@@ -2,6 +2,8 @@ package optional;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Validation {
 	public static void main(String[] args) {
 
@@ -13,9 +15,40 @@ public class Validation {
 		// 3. Use the randomNumber to give the user a random compliment.
 
 		// 4. Repeat all the code above 10 times
+		for (int i = 0; i < 10; i++) {
+			if (randomNumber == 1) {
+				JOptionPane.showMessageDialog(null, "You look very pretty today!");
+			} 
+			else if(randomNumber == 2) {
+				JOptionPane.showMessageDialog(null, "You have a nice suit!");
+			} 
+			else if(randomNumber == 3) {
+				JOptionPane.showMessageDialog(null, "I think you have a nice smile. Let me see you smile!");
+			}
+			else if(randomNumber == 4) {
+				JOptionPane.showMessageDialog(null, "I think you have a beautiful eyes. Am I right?");
+			} 
+			else if(randomNumber == 5) {
+				JOptionPane.showMessageDialog(null, "I think you are awesome.");
+			} 
+			else {
+				JOptionPane.showMessageDialog(null, "I think you have a nice hearcut.");
+				
+			}
+			
+		
+		
+		
+		}
+		
 		
 		// 5. Find someone to test out your program. They will like it :)
 	}
 }
+		
+		
+		
+		
+		
 
 
