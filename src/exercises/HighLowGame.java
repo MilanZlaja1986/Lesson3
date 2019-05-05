fd48847f06dcfc0341af5d5e7b07387738db0753
MilanZlaja1986 <milan.zlatkovic@hotmail.rs>
2019-05-05 13:57:27 +0200

    Second change

--- src/exercises/HighLowGame.java
+++ src/exercises/HighLowGame.java
@@ -1,6 +1,37 @@
 package exercises;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
+	
+	public static void main(String[] args) {
+		
+		Random random = new Random();
+		int randomNumber = random.nextInt(101);
+		
+		boolean userGuess = false;
+		String userInputString;
+		int userNumber;
+		
+		while(!userGuess)
+		{
+			userInputString = JOptionPane.showInputDialog("Guess the number ");
+			userNumber = Integer.parseInt(userInputString);
+			if(userNumber > randomNumber){
+				JOptionPane.showInputDialog(null, "High, try again.");	
+			} else if (userNumber < randomNumber){
+				JOptionPane.showInputDialog(null, "Low, try again.");
+			} else {
+				JOptionPane.showInputDialog(null, "Congradulations, you guessed the number.");
+				userGuess = true;				
+			}
+			
+		}
+		System.exit(0);
+
+	}
 
 
 }
