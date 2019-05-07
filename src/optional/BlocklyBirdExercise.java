bb4f4f67465d5e03d212c4d6932c9636aa927d7f
MilanZlaja1986 <milan.zlatkovic@hotmail.rs>
2019-05-07 20:09:48 +0200

    Second change

--- src/optional/BlocklyBirdExercise.java
+++ src/optional/BlocklyBirdExercise.java
@@ -4,4 +4,26 @@ package optional;
 *			bit.ly/bird-puzzle
 */
 
+/*
+ * 1. heading(45);
+ * 
+ * 2.if (noWorm()) {
+  heading(0);
+} else {
+  heading(90);
+}
+
+*3. if (noWorm()) {
+  heading(300);
+} else {
+  heading(60);
+}
+
+*4.if (getX() < 80) {
+  heading(0);
+} else {
+  heading(270);
+}
+ */
+
 public class BlocklyBirdExercise {}
